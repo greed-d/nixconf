9180e9a3a47827e161c2987d656db2d11193a2fc
greed-d <deekshit.bti@gmail.com>
2025-04-15 22:05:42 +0545

    `rem` : remove home-manager-backupfile

--- home/core.nix
+++ home/core.nix
@@ -1,13 +1,9 @@
-{ username, ... }: {
+{ username, config, ... }: {
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
   };
   home.stateVersion = "24.11";
+  home.sessionPath = [ "/home/greed/.local/scripts" ];
   programs.home-manager.enable = true;
-  # programs.home-manager.backupFileExtension = "bak";
-  # programs.home-manager = {
-  #   enable = true;
-  #   backupFileExtension = "backup";
-  # };
 }
