dd9dd36d91577087db9ecad22682f8259e9e6a02
greed-d <deekshit.bti@gmail.com>
2025-04-15 16:47:24 +0545

    `chore` : add fuzzel

---
+++ home/programs/fuzzel.nix
@@ -0,0 +1,36 @@
+{ inputs, pkgs, config, ... }: {
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        anchor = "left";
+        prompt = "       ";
+        layer = "overlay";
+        x-margin = 10;
+        y-margin = 10;
+        lines = 23;
+        width = 30;
+        tabs = 8;
+        placeholder = "Search";
+        icon-theme = "Papirus-Dark";
+        font = "JetBrainsMono Nerd Font-8";
+        line-height = 20;
+      };
+      colors = {
+        background = "16161eff";
+        text = "c0caf5ff";
+        match = "2ac3deff";
+        # text = "cdd6f4ff";
+        selection = "343a55ff";
+        selection-match = "2ac3deff";
+        selection-text = "c0caf5ff";
+        border = "45475aff";
+      };
+
+      border = {
+        width = 2;
+        radius = 10;
+      };
+    };
+  };
+}
