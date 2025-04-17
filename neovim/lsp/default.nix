03709295e6707bc5501e696013ccccbce1266319
greed-d <deekshit.bti@gmail.com>
2025-04-17 20:45:48 +0545

    `refac` : change default lsp bindings

--- neovim/lsp/default.nix
+++ neovim/lsp/default.nix
@@ -1,10 +1,20 @@
-{ ... } : {
-  imports = [ ./nix.nix ];
+{...}: {
   vim = {
+    lsp.formatOnSave = true;
     languages = {
       enableLSP = true;
       enableTreesitter = true;
+      nix = import ./nix.nix;
+      python = import ./python.nix;
+    };
+    lsp.mappings = {
+      goToDeclaration = "gD";
+      goToDefinition = "gd";
+      goToType = "gy";
+      nextDiagnostic = "]d";
+      previousDiagnostic = "[d";
+      listImplementations = "gI";
+      listReferences = "gr";
     };
   };
-
 }
