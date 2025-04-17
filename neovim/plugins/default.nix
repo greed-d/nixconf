685ae396ca8ac22338cc2d88a8c458872df165b4
greed-d <deekshit.bti@gmail.com>
2025-04-18 02:45:54 +0545

    `chore` : add mini plugins suite

--- neovim/plugins/default.nix
+++ neovim/plugins/default.nix
@@ -1,3 +1,3 @@
 {...}: {
-  imports = [./snacks ./lualine.nix ./telescope.nix ./conform.nix ./autopairs.nix ./blink.nix ./neotree.nix ./bufferline.nix ./gitsigns.lua];
+  imports = [./snacks ./lualine.nix ./telescope.nix ./conform.nix ./autopairs.nix ./blink.nix ./neotree.nix ./bufferline.nix ./gitsigns.nix ./mini.nix];
 }
