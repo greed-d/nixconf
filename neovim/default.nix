f5969efe71adeb8dabe3fd87be6966dc9d69f7e7
greed-d <deekshit.bti@gmail.com>
2025-04-17 20:47:22 +0545

    `feat` : make snacks picker look like telescope

--- neovim/default.nix
+++ neovim/default.nix
@@ -1,10 +1,43 @@
-{ pkgs, lib, ... }: {
-  imports = [ ./plugins ./lsp ./core ];
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [./plugins ./lsp ./core];
   config.vim = {
     theme = {
       enable = true;
       name = "tokyonight";
       style = "night";
+    };
+    highlight = {
+      FloatBorder = {
+        fg = "#45475A";
+        bg = null;
+      };
+      SnacksPickerTitle = {
+        bg = "#7aa2f7";
+        fg = "#1f2335";
+      };
+      SnacksPickerPreview = {bg = "#1a1b26";};
+      SnacksPickerList = {bg = "#1a1b26";};
+      SnacksPickerListTitle = {
+        bg = "#9ece6a";
+        fg = "#1f2335";
+      };
+      SnacksPickerInputTitle = {
+        bg = "#f7768e";
+        fg = "#1f2335";
+      };
+      SnacksPickerInputBorder = {
+        bg = "#1a1b26";
+        fg = "#45475a";
+      };
+      SnacksPickerInputSearch = {
+        bg = "#f7768e";
+        fg = "#1f2335";
+      };
+      SnacksPickerInput = {bg = "#1a1b26";};
     };
   };
 }
