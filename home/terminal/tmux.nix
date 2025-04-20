e2fe7d9bf432b50d92a3d95314ee10a7aff5f266
greed-d <deekshit.bti@gmail.com>
2025-04-20 21:00:18 +0545

    `chore` : remove unused lines

--- home/terminal/tmux.nix
+++ home/terminal/tmux.nix
@@ -124,7 +124,6 @@
             rev = "48fae59ba4503cf345d25e4e66d79685aa3ceb75";
             sha256 = "sha256-yKMTuRiNnGC3tAbZeWzMEudzhBSJhji8sZh96rFxpb8=";
           };
-          extraConfig = "";
         };
       }
       {
